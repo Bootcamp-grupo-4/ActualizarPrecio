5caf51ac0dfc631c4cb7283df5b0cfe7da231c09
Sergio Salgueiro Monforte <90696985+Sergiosalgueiro01@users.noreply.github.com>
2024-10-22 15:21:58 +0200

    Add files via upload

---
+++ src/main/java/com/capgeticket/actualizarprecios/Actualizar/precios/ActualizarPreciosApplication.java
@@ -0,0 +1,13 @@
+package com.capgeticket.actualizarprecios.Actualizar.precios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualizarPreciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualizarPreciosApplication.class, args);
+	}
+
+}
